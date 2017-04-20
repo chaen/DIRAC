e404cd66376b32841de0388ca9d7cb2bc1dea761
Unknown <alex.811@hotmail.fr>
2017-04-20 16:59:30 +0200

    delete some useless files

--- TestLoggerSystem/Client/HelloClient.py
+++
@@ -1,8 +0,0 @@
-from DIRAC.Core.DISET.RPCClient import RPCClient
-
-simpleMessageService = RPCClient('TestLogger/Hello')
-result = simpleMessageService.sayHello( 'you' )
-if not result['OK']:
-  print "Error while calling the service:", result['Message'] #Here, in DIRAC, you better use the gLogger
-else:
-  print result[ 'Value' ] #Here, in DIRAC, you better use the gLogger
