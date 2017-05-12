9d7d0a11a835174748df55bfd327322a778bc15b
Unknown <alex.811@hotmail.fr>
2017-05-12 14:06:55 +0200

    Fix protected calls problem from Logger

---
+++ FrameworkSystem/private/standardLogging/LogLevels.py
@@ -0,0 +1,32 @@
+import logging
+
+
+class LogLevels:
+
+  def __init__(self):
+    self.__levelDict = {"always": logging.INFO,
+                        "notice": logging.INFO,
+                        "info": logging.INFO,
+                        "verbose": logging.INFO,
+                        "debug": logging.DEBUG,
+                        "warn": logging.WARN,
+                        "exception": logging.ERROR,
+                        "error": logging.ERROR,
+                        "fatal": logging.CRITICAL}
+
+  def getLevelValue(self, sName):
+    if self.__levelDict.has_key(sName):
+      return self.__levelDict[sName]
+    else:
+      return None
+
+  def getLevel(self, level):
+    """ Get level name given the level digital value 
+    """
+    for lev in self.__levelDict:
+      if self.__levelDict[lev] == level:
+        return lev
+    return "Unknown"
+
+  def getLevels(self):
+    return self.__levelDict.keys()
