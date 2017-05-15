ed77fe67e05a9d9e2f2b061582c7210a089e1d88
Unknown <alex.811@hotmail.fr>
2017-05-15 16:16:59 +0200

    Delete calls to protected attributes of gLogger

--- FrameworkSystem/private/standardLogging/gLogger.py
+++ FrameworkSystem/private/standardLogging/gLogger.py
@@ -5,7 +5,9 @@ from DIRAC.FrameworkSystem.private.standardLogging.LogLevels import LogLevels
 
 class gLogger():
   """
-  Wrapper of the old gLogger object
+  Wrapper of the old gLogger object:
+  - made to replace transparently gLogger
+  - offer standard logging functionalities
   """
 
   _initializedLogging = False
@@ -13,64 +15,98 @@ class gLogger():
   _levels = LogLevels()
 
   def __init__(self, name=''):
-
-    self._logLevels = gLogger._levels
-    self._outputList = []
-
     self.logger = logging.getLogger(name)
 
     if not gLogger._initializedLogging:
       LoggingConfiguration.initializeLogging()
-
-      self._minLevel = logging.getLogger().getEffectiveLevel()
-      self._systemName = LoggingConfiguration.componentName
-
       gLogger._initializedLogging = True
-    else:
-      self._minLevel = logging.getLogger().getEffectiveLevel()
-      self._systemName = LoggingConfiguration.componentName
 
   def initialized(self):
     return _initializedLogging
 
   def showHeaders(self, yesno=True):
+    """
+    Depending on the value, display or not the prefix of the message
+    input: 
+    - yesno: boolean determining the behaviour of the display
+    """
     LoggingConfiguration.showHeaders(yesno)
 
   def showThreadIDs(self, yesno=True):
+    """
+    Depending on the value, display or not the thread ID
+    input: 
+    - yesno: boolean determining the behaviour of the display
+    """
     LoggingConfiguration.showThreadIDs(yesno)
 
   def registerBackends(self, desiredBackends):
     logging.info("Logging register its backends itself.")
 
   def initialize(self, systemName, cfgPath):
+    """
+    Configure gLogger depending on the component used
+    input:
+    - systemName: string represented as "system name/component name"
+    - cfgPath: string of the cfg file path
+    """
     if not gLogger._configuredLogging:
       LoggingConfiguration.configureLogging(systemName, cfgPath)
-
-      self._minLevel = logging.getLogger().getEffectiveLevel()
-      self._systemName = LoggingConfiguration.componentName
 
       gLogger._configuredLogging = True
 
   def setLevel(self, levelName):
+    """
+    Set a level to the logger
+    input:
+    - levelName: string representing the level to give to the logger
+    output:
+    - result: boolean representing if the setting is done or not
+    """
     result = False
+    levelName = levelName.upper()
     if levelName in gLogger._levels.getLevels():
       self.logger.setLevel(gLogger._levels.getLevelValue(levelName))
-      self._minLevel = gLogger._levels.getLevelValue(levelName)
-
       result = True
     return result
 
   def getLevel(self):
-    return self.logger.getEffectiveLevel()
+    """
+    Return the name of the level
+    """
+    return gLogger._levels.getLevel(self.logger.getEffectiveLevel())
 
   def shown(self, levelName):
+    """
+    Determine if messages with a certain level will be displayed or not
+    input: 
+    - levelName: string representing the level to analyse
+    output:
+    - result : boolean which give the answer
+    """
     result = False
     if levelName in gLogger._levels.getLevels():
-      result = self.logger.isEnabledFor(gLogger._levels.getLevelValue(levelName))
+      result = self.logger.isEnabledFor(
+          gLogger._levels.getLevelValue(levelName))
     return result
 
   def getName(self):
-    return self._systemName
+    """
+    Return "system name/component name"
+    """
+    return LoggingConfiguration.componentName
+
+  def getSubName(self):
+    """
+    Return the name of the logger
+    """
+    return self.logger.name
+
+  def getAllPossibleLevels(self):
+    """
+    Return a list of all levels available
+    """
+    return gLogger._levels.getLevels()
 
   def always(self, sMsg, sVarMsg=''):
     self.logger.info("%s %s" % (sMsg, sVarMsg))
@@ -109,4 +145,9 @@ class gLogger():
     logging.info("Logging flush all messages itself.")
 
   def getSubLogger(self, subName, child=True):
+    """
+    Create a new gLogger object, child of this logger
+    input: 
+    - subName: the name of the child
+    """
     return gLogger(subName)
