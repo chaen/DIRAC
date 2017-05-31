1bc87d2cf1ae9dcd8c30030564ba0de60509f1a8
Unknown <alex.811@hotmail.fr>
2017-05-31 12:04:40 +0200

    Improve BaseFormatter.py

--- FrameworkSystem/private/standardLogging/Formatter/BaseFormatter.py
+++ FrameworkSystem/private/standardLogging/Formatter/BaseFormatter.py
@@ -9,8 +9,10 @@ import logging
 
 class BaseFormatter(logging.Formatter):
   """
-  Formatter of logging:
-  - format messages to correspond with the gLogger format.
+  BaseFormatter is used to format log record to create a string representing a log message. 
+  It is based on the Formatter object of the standard logging library.
+
+  This custom formatter is useful for format messages to correspond with the gLogger format.
   """
 
   def __init__(self, fmt, datefmt, options):
@@ -26,25 +28,34 @@ class BaseFormatter(logging.Formatter):
 
   def format(self, record):
     """
-    Overriding
+    Overriding. 
+    format is the main method of the Formatter object because it is the method which transforms 
+    a log record into a string. 
 
-    Format for DIRAC use: add the "system/component" to the logname.
+    In this case, the format method change the record name to replace "root" by nothing to avoid
+    to pollute the display. 
+    In fact, "root" corresponds to the first logger and can appears in all messages. 
+
+    Moreover, the format method change "." by "/" in the log chain.
+
+    For example : Framework/Atom/root.log.sublog will become Framework/Atom/log/sublog
+
+    :params record: the log record containing all the information about the log message: name, level, threadid...
     """
-    # pre treatment
+    # create a record name copy
     logname = record.name
 
-    if record.name == "root":
-      record.name = ""
-    else:
-      if record.name.startswith("root."):
-        record.name = record.name.replace("root.", "")
-        record.name = record.name.replace(".","/")
-      record.name = "/" + record.name
+    # format the record name removing "root", replacing "." by "/"
+    record.name = record.name.replace("root", "")
 
+    if record.name != "":
+      record.name = record.name.replace(".", "/")
+
+    # format the record
     stringRecord = super(BaseFormatter, self).format(record)
 
-    # this is important because logger creates the record and the same
-    # record is used by different handlers
+    # restore the record name with the copy
+    # useful because the same record name is used by different handlers and it does not be changed.
     record.name = logname
 
     return stringRecord
