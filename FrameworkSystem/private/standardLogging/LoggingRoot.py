4b553d2feae82b8ccb0d46a3e5eafd83bb1d074c
Unknown <alex.811@hotmail.fr>
2017-06-08 16:22:36 +0200

    Precise a comment on the remove of stdout in LoggingRoot

--- FrameworkSystem/private/standardLogging/LoggingRoot.py
+++ FrameworkSystem/private/standardLogging/LoggingRoot.py
@@ -86,9 +86,11 @@ class LoggingRoot(Logging):
       backends = (None, None)
       Logging._componentName = systemName
 
-      # Remove stdout from the list of backends
+      # Remove all the backends from the list of backends as in the old gLogger.
+      # this can be useful to have logs only in a file for instance.
       del self._backendsList[:]
       self._logger.removeHandler(self._logger.handlers[0])
+
       # Backend options
       desiredBackends = gConfig.getValue("%s/LogBackends" % cfgPath, ['stdout'])
 
