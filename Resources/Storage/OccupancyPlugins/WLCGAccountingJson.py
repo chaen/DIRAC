66a01901c4df80f4f4697cf3d4de1ecb9079c9ff
hirata <hirata@hepl.phys.nagoya-u.ac.jp>
2019-08-01 19:53:32 +0900

    replace python WLCGAccountingJson2.py to WLCGAccountingJson.py and change method to copy json file

--- Resources/Storage/OccupancyPlugins/WLCGAccountingJson.py
+++ Resources/Storage/OccupancyPlugins/WLCGAccountingJson.py
@@ -2,10 +2,10 @@
   Defines the plugin to take storage space information given by WLCG Accounting Json
   https://twiki.cern.ch/twiki/bin/view/LCG/AccountingTaskForce#Storage_Space_Accounting
   https://twiki.cern.ch/twiki/pub/LCG/AccountingTaskForce/storage_service_v4.txt
-  https://docs.google.com/document/d/1yzCvKpxsbcQC5K9MyvXc-vBF1HGPBk4vhjw3MEXoXf8/edit
+  https://docs.google.com/document/d/1yzCvKpxsbcQC5K9MyvXc-vBF1HGPBk4vhjw3MEXoXf8
 """
 import json
-import gfal2
+import gfal2  # pylint: disable=import-error
 import os
 import tempfile
 import shutil
@@ -22,76 +22,85 @@ class WLCGAccountingJson(object):
   def __init__(self, se):
     self.se = se
     self.log = se.log.getSubLogger('WLCGAccountingJson')
-
-    # assume given SE speaks SRM
-    ret = se.getStorageParameters(protocol='srm')
-    if not ret['OK']:
-      self.log.error(ret['Message'])
-      return ret
-    self.storageParameters = ret['Value']
+    self.name = self.se.name
 
   def getOccupancy(self, **kwargs):
     """ Returns the space information given by LCG Accouting Json
 
         :returns: S_OK with dict (keys: Total, Free)
     """
-    if 'StorageName' not in self.storageParameters:
-      return S_ERROR('No storage name cannot be retrieve.')
-    sename = self.storageParameters['StorageName']
+    spaceReservation = self.se.options.get('SpaceReservation')
+    if not spaceReservation:
+      self.log.debug("Check SpaceToken instead of SpaceReservation because it is not defined in CS")
+      for storage in self.se.storages:
+        SEparams = storage.getParameters()
+        if not SEparams:
+          return S_ERROR('Could not get storage parameters at %s' % (self.name))
+        if 'SpaceToken' not in SEparams:
+          return S_ERROR('Could not find SpaceToken key in storage parameters at %s' % (self.name))
+        spaceReservation = SEparams['SpaceToken']
 
-    if 'SpaceToken' not in self.storageParameters:
-      return S_ERROR('Could not find SpaceToken key in StorageParameters of %s' % sename)
-    spacetoken = self.storageParameters['SpaceToken']
-
-    occupancyLFN = kwargs['occupancyLFN']
+    # occupancyLFN = '/dpm/na.infn.it/home/belle/storagesummary.json'
 
     if not occupancyLFN:
-      return S_ERROR("Failed to get LFN of occupancy json file")
+      return S_ERROR("Failed to get occupancyLFN")
 
-    try:
-      # get a json file
-      tmpDirName = tempfile.mkdtemp()
-      res = self.se.getFile(occupancyLFN, localPath=tmpDirName)
+    tmpDirName = tempfile.mkdtemp()
+    filePath = os.path.join(tmpDirName, os.path.basename(occupancyLFN))
 
-      if not res['OK']:
-        return res
+    for storage in self.se.storages:
+      try:
+        ctx = gfal2.creat_context()
+        params = ctx.transfer_parameters()
+        params.overwrite = True
+        res = storage.updateURL(occupancyLFN)
+        if not res['OK']:
+          return res
+        occupancyURL = res['Value']
+        ctx.filecopy(params, occupancyURL, 'file://' + filePath)
 
-      # load the json file
-      filePath = os.path.join(tmpDirName, os.path.basename(occupancyLFN))
-      with open(filePath, 'r') as occupancyFile:
-        occupancyDict = json.load(occupancyFile)
-
-      if 'storageservice' not in occupancyDict:
-        return S_ERROR('Could not find storageservice component in %s at %s' % (occupancyLFN, sename))
-      storageservice = occupancyDict['storageservice']
-      if 'storageshares' not in storageservice:
-        return S_ERROR('Could not find storageshares component in %s at %s' % (occupancyLFN, sename))
-      storageshares = occupancyDict['storageservice']['storageshares']
-
-    except Exception as e:
-      return S_ERROR(repr(e))
-
-    finally:
-      # delete temp dir
-      shutil.rmtree(tmpDirName)
-
-    storagesharesST = None
-    for key in storageshares:
-      if key['name'] != spacetoken:
+      except gfal2.GError as e:
+        detailMsg = "Failed to copy file %s to destination url %s: [%d] %s" % (
+            occupancyURL, filePath, e.code, e.message)
+        self.log.debug("Exception while copying", detailMsg)
         continue
-      storagesharesST = key
 
-    if not storagesharesST:
+      else:
+        break
+
+    if not os.path.isfile(filePath):
+      return S_ERROR('No WLCGAccountingJson file of %s is downloaded.' % (self.name))
+
+    with open(filePath, 'r') as path:
+      occupancyDict = json.load(path)
+
+    # delete temp dir
+    shutil.rmtree(tmpDirName)
+
+    if 'storageservice' not in occupancyDict:
+      return S_ERROR('Could not find storageservice component in %s at %s' % (occupancyLFN, self.name))
+    storageService = occupancyDict['storageservice']
+
+    if 'storageshares' not in storageService:
+      return S_ERROR('Could not find storageshares component in %s at %s' % (occupancyLFN, self.name))
+    storageShares = occupancyDict['storageservice']['storageshares']
+
+    storageSharesSR = None
+    for key in storageShares:
+      if key['name'] == spaceReservation:
+        storageSharesSR = key
+        break
+    if not storageSharesSR:
       return S_ERROR('Could not find %s component in storageshares of %s at %s' % (
-          spacetoken, occupancyLFN, sename))
+          spaceReservation, occupancyLFN, self.name))
 
     sTokenDict = {}
-    if 'totalsize' not in storagesharesST:
-      return S_ERROR('Could not find totalsize key in storageshares')
-    sTokenDict['Total'] = storagesharesST['totalsize']
+    if 'totalsize' not in storageSharesSR:
+      return S_ERROR('Could not find totalsize key in %s storageshares' % spaceReservation)
+    sTokenDict['Total'] = storageSharesSR['totalsize']
 
-    if 'usedsize' not in storagesharesST:
-      return S_ERROR('Could not find usedsize key in storageshares')
-    sTokenDict['Free'] = sTokenDict['Total'] - storagesharesST['usedsize']
+    if 'usedsize' not in storageSharesSR:
+      return S_ERROR('Could not find usedsize key in %s storageshares' % spaceReservation)
+    sTokenDict['Free'] = sTokenDict['Total'] - storageSharesSR['usedsize']
 
     return S_OK(sTokenDict)
