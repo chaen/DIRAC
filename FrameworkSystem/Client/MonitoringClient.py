a095ba26ab089677416edcd78e6ad8aacf0f133b
Christophe Haen <christophe.haen@cern.ch>
2020-06-05 15:02:30 +0200

    fix pylint

--- FrameworkSystem/Client/MonitoringClient.py
+++ FrameworkSystem/Client/MonitoringClient.py
@@ -5,7 +5,7 @@
 __RCSID__ = "$Id"
 
 import time
-
+import os
 import six
 
 import DIRAC
@@ -82,7 +82,12 @@ class MonitoringFlusher(object):
       self.__mcList.append(mc)
 
 
-gMonitoringFlusher = MonitoringFlusher()
+# USE_TORNADO_IOLOOP is defined by starting scripts
+if os.environ.get('USE_TORNADO_IOLOOP', 'false').lower() == 'true':
+  from DIRAC.FrameworkSystem.Client.MonitoringClientIOLoop import MonitoringFlusherTornado
+  gMonitoringFlusher = MonitoringFlusherTornado()
+else:
+  gMonitoringFlusher = MonitoringFlusher()
 
 
 class MonitoringClient(object):
@@ -101,6 +106,7 @@ class MonitoringClient(object):
   COMPONENT_AGENT = "agent"
   COMPONENT_WEB = "web"
   COMPONENT_SCRIPT = "script"
+  COMPONENT_TORNADO = "tornado"
 
   __validMonitoringValues = six.integer_types + (float,)
 
@@ -174,6 +180,8 @@ class MonitoringClient(object):
       self.setComponentName('WebApp')
     elif self.sourceDict['componentType'] == self.COMPONENT_SCRIPT:
       self.cfgSection = "/Script"
+    elif self.sourceDict['componentType'] == self.COMPONENT_TORNADO:
+      self.cfgSection = "/Tornado"
     else:
       raise Exception("Component type has not been defined")
     gMonitoringFlusher.registerMonitoringClient(self)
